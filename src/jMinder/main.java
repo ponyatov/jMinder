ee5de31c12d101c6ba237ba65063b5303e6a7222
Dmitry Ponyatov <dponyatov@gmail.com>
2013-03-22 03:59:26 +0400

    * CLDC-1.0 MIDP-1.0 + icons 32x32 * simplest main form works
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/jMinder/main.java
+++ src/jMinder/main.java
@@ -10,49 +10,57 @@ import javax.microedition.midlet.MIDletStateChangeException;
 
 public class main extends MIDlet implements CommandListener {
 
-	Command cmdExit = new Command("Exit", Command.EXIT, 2);
-	Command cmdNew = new Command("New", Command.OK, 1);
-	Form frmMain = new Form("jMinder");
+	Display display = Display.getDisplay(this);
 
+	Form frmMain = new Form("jMinder");
+	
+	Command cmdExit   = new Command("Exit", Command.EXIT, 0);
+	Command cmdNew    = new Command("New", Command.OK, 0);
+	Command cmdEdit   = new Command("Edit", Command.OK, 0);
+	Command cmdGet    = new Command("Get", Command.OK, 0);
+	Command cmdDone   = new Command("Done", Command.OK, 0);
+	Command cmdRemove = new Command("Remove", Command.OK, 0);
+	
 	public main() {
-//		Alert helloAlert = new Alert("Testing", "Hello, world!", null, AlertType.INFO);
-//		helloAlert.setTimeout(Alert.FOREVER);
-		frmMain.setCommandListener(this);
-		frmMain.append("Hello World !");
-		// add
-		frmMain.addCommand(cmdNew);
-		// exit
-		frmMain.addCommand(cmdExit);
-		// get display
-		Display display = Display.getDisplay(this);
-		// display form
-		display.setCurrent(frmMain);
+	// TODO Auto-generated constructor stub
 	}
 
-	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
+	protected void destroyApp(boolean unconditional)
+		throws MIDletStateChangeException {
 		// TODO Auto-generated method stub
 		notifyDestroyed();
 	}
 
+
 	protected void pauseApp() {
 		// TODO Auto-generated method stub
-
 	}
 
 	protected void startApp() throws MIDletStateChangeException {
 		// TODO Auto-generated method stub
-
+		// add menu commands
+		frmMain.addCommand(cmdNew);
+		frmMain.addCommand(cmdEdit);
+		frmMain.addCommand(cmdGet);
+		frmMain.addCommand(cmdDone);
+		frmMain.addCommand(cmdRemove);
+		frmMain.addCommand(cmdExit);
+		// forming task list
+		
+		// set listener
+		frmMain.setCommandListener(this);
+		// show form
+		display.setCurrent(frmMain);
 	}
 
 	public void commandAction(Command cmd, Displayable disp) {
 		// TODO Auto-generated method stub
-		if (cmd==cmdExit)
-			try {
-				destroyApp(false);
-			} catch (MIDletStateChangeException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+		if (cmd==cmdExit) destroyApp(false);
+		} catch (MIDletStateChangeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
