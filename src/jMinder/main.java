b5d464fec2d1353d06cdc40646ae8a9aecbc6f58
Dmitry Ponyatov <dponyatov@gmail.com>
2013-03-24 17:53:41 +0400

    tiny works: shows date time and tasks (noneditable)

--- src/jMinder/main.java
+++ src/jMinder/main.java
@@ -1,95 +1,99 @@
 package jMinder;
 
+import java.util.Date;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.DateField;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.List;
+import javax.microedition.lcdui.StringItem;
 import javax.microedition.midlet.MIDlet;
-import com.sun.lwuit.Display;
-import com.sun.lwuit.Form;
 
-public class main extends MIDlet {
+public class main extends MIDlet implements CommandListener {
 
-	public void destroyApp(boolean unconditional) { 
-//		throws MIDletStateChangeException {
-		notifyDestroyed(); 
-	} 
+	public void destroyApp(boolean unconditional) { notifyDestroyed(); }
 
-	public void pauseApp() {}
-
-	public void startApp() { // throws MIDletStateChangeException {
-		// TODO Auto-generated method stub
-		Display.init(this);
-		Form frmMain = new Form("jMinder");
-		frmMain.show();
-	}
-}
-
-/*
-public class main extends MIDlet { // implements CommandListener {
-
+	public void pauseApp() { }
+	
 	Display display = Display.getDisplay(this);
-
-	Command cmdNew    = new Command("New", Command.EXIT, 0);
-	Command cmdDone   = new Command("Done", Command.OK, 9);
-	Command cmdEdit   = new Command("Edit", Command.OK, 7);
-	Command cmdRemove = new Command("Remove", Command.OK, 5);
-	Command cmdSync   = new Command("Sync", Command.OK, 3);
-	Command cmdExit   = new Command("Exit", Command.OK, 0);
+	Form frmMain = new Form("jMinder");
+	Command cmdNew = new Command("New", Command.EXIT, 0);
+	Command cmdExit = new Command("Exit", Command.OK, 0);
 	
-	String[] pool = {
-			"X","Y","J",
-			"X","Y","J",
-			"X","Y","J",
-			"X","Y","J",
-			"X","Y","J",
-			"X","Y","J",
-			"X","Y","J"
-			};
+	Form frmNew = new Form("new");
+	Command cmdOK = new Command("OK", Command.OK, 0);
+	Command cmdCancel = new Command("Cancel", Command.CANCEL, 0);
+
+	DateField fldDateTime = new DateField("Now", DateField.DATE_TIME);
 	
-	Calendar calendar = Calendar.getInstance();
-	DateField datefield = new DateField("DF", DateField.DATE_TIME);
+	String[] Tasks = {"jMinder","Cortex Book","Milling"};
 	
-	public main() {
-	// TODO Auto-generated constructor stub
-	}
-
-	protected void destroyApp(boolean unconditional)
-		throws MIDletStateChangeException {
-		// TODO Auto-generated method stub
-		notifyDestroyed();
-	}
-
-
-	protected void startApp() throws MIDletStateChangeException {
-		// TODO Auto-generated method stub
-		// add menu commands
+	public void startApp() { // throws MIDletStateChangeException {
+		// def menus
+		// main
 		frmMain.addCommand(cmdNew);
-		frmMain.addCommand(cmdDone);
-		frmMain.addCommand(cmdEdit);
-		frmMain.addCommand(cmdSync);
-		frmMain.addCommand(cmdRemove);
 		frmMain.addCommand(cmdExit);
-		// forming screen header
-		datefield.setDate(calendar.getTime());
-		frmMain.append(datefield);
-		frmMain.append(calendar.getTime().toString());
-		
-		frmMain.append("\n"+cv.w()+"x"+cv.h()+"\n");
-		
-		// forming task list
-		for (int i=0;i<11;i++) frmMain.append(pool[i]+"\n");
-		// set listener
 		frmMain.setCommandListener(this);
-		// show form
+		frmMain.append(fldDateTime);
+		fldDateTime.setDate(new Date());
+		for (int i=0;i<Tasks.length;i++) frmMain.append(Tasks[i]+"\n");
+		// new
+		frmNew.addCommand(cmdOK);
+		frmNew.addCommand(cmdCancel);
+		frmNew.setCommandListener(this);
+		// show main form
 		display.setCurrent(frmMain);
 	}
 
-	public void commandAction(Command cmd, Displayable disp) {
-		// TODO Auto-generated method stub
-		try {
-		if (cmd==cmdExit) destroyApp(false);
-		} catch (MIDletStateChangeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void commandAction(Command c, Displayable d) {
+		if (d==frmMain) {
+			if (c==cmdExit) destroyApp(false);
+			if (c==cmdNew) display.setCurrent(frmNew);
+		}
+		if (d==frmNew) {
+			if (c==cmdOK) {}
+			if (c==cmdCancel) {}
+			display.setCurrent(frmMain);
 		}
 	}
 
 }
+
+/*
+
+ // implements ActionListener {
+
+	public void destroyApp(boolean arg0) { notifyDestroyed(); }
+	public void pauseApp() { }
+	
+	Command cmdExit;
+	
+	public void startApp() { // throws MIDletStateChangeException {
+		Display.init(this);
+		// main form
+		Image bgImage=null;
+		try {
+			 bgImage = Image.createImage("/lenin-240x320.png");
+		} catch (IOException e) {e.printStackTrace();}
+		frmMain.getStyle().setBgImage(bgImage);
+		// menu
+		frmMain.addCommand(cmdExit);
+		frmMain.l
+		// time bar
+		Date date = new Date();
+		Label lblTime = new Label(date.toString());
+		frmMain.addComponent(lblTime);
+		// show form
+		frmMain.show();
+	}
+	
+	public void actionPerformed(ActionEvent event) {
+		Command c = event.getCommand();
+		if (c==cmdExit) destroyApp(true);
+	}
+}
+
 */
